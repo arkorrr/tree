b422559cc28ca7adf6a530be507daa1d9fa6aeb6
trakt <trakt@DESKTOP-UDGVRCB>
2024-02-02 19:57:47 +0200

    Добавьте файлы проекта.

---
+++ tree/Node.cpp
@@ -0,0 +1,21 @@
+﻿#include "Node.h"
+
+char* Node::GetLastName()
+{
+    return lastname;
+}
+
+char* Node::GetName()
+{
+    return name;
+}
+
+char* Node::GetSurname()
+{
+    return surname;
+}
+
+char* Node::GetMark()
+{
+    return mark;
+}
---
+++ tree/Node.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+struct Node
+{
+    char lastname[15];
+	char name[15];
+    char surname[15];
+    char mark[15];
+
+	Node* m_parent;
+
+    Node* m_left;  
+    Node* m_right;
+	
+
+    char* GetLastName();
+    char* GetName();
+    char* GetSurname();
+    char* GetMark();
+};
